ed2dc01aa562b4b36714969db588f2ed90807d4c
MaxHaPa6oTe <ya.bulankin-2015@yandex.ru>
2023-06-07 16:53:52 +0300

    she

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,48 @@
 import PostModel from '../models/Post.js';
 
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec()
+
+        res.json(posts)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось получить статьи'
+        })
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id
+        PostModel.findOneAndDelete({
+            _id: postId,
+        }, (err, doc) => {
+            if (err) {
+                console.log(err)
+        res.status(500).json({
+            message: 'Не удалось удалить статьи'
+        })
+            }
+            if (!doc) {
+                res.status(500).json({
+                    message: 'Статья не найдена',
+                })
+            }
+            res.json({
+                success: 'true'
+            })
+        })
+        
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось получить статьи'
+        })
+    }
+}
+
 export const create = async (req, res) => {
     try {
          const doc = new PostModel({
--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.post('/auth/register', registerValidation, UserController.register);
 
 app.get('/auth/me', checkAuth, UserController.getMe)
 
-// app.get('/posts', PostController.getAll)
-// app.get('/posts/:id', PostController.getOne)
-app.post('/posts', postCreateValidation, PostController.create)
-// a pp.get('/posts', PostController.remove)
+/app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+app.get('/posts', checkAuth, PostController.remove)
 // app.get('/posts', PostController.update)
 
 app.listen(4444, (err) => {
